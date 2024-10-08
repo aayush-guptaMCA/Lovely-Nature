6e18a094c146c0885db62c79114938249beea4fd
Aayush Gupta <132879888+aayush-guptaMCA@users.noreply.github.com>
2024-10-08 23:19:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.right')
+
+burger.addEventListener('click',()=>{
+    right.classList.toggle('v-class-resp');
+navbar.classList.toggle('h-nav-resp');
+})
